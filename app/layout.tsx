8ae7a898f8973a3772454fc89729cb015e37ac2e
Quentin Smith <quentin@shockedfuturestudios.com>
2025-12-02 16:31:44 -0600

    Add Adsense & Opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from '@/components/Footer';
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GoogleTagManager } from '@next/third-parties/google'
 import { AuthProvider } from '@/context/AuthContext';
+import { GoogleAdSense } from '@/components/GoogleAdsense';
 
 
 const inter = Host_Grotesk({ subsets: ['latin'] });
@@ -13,6 +14,22 @@ const inter = Host_Grotesk({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Shocked Future',
   description: 'Reengineering yesterday\'s classics for tomorrow\'s gamers.',
+  openGraph: {
+    title: 'Shocked Future',
+    description: 'Reengineering yesterday\'s classics for tomorrow\'s gamers.',
+    url: 'https://www.shockedfuturestudios.com',
+    siteName: 'Shocked Future',
+    images: [
+      {
+        url: 'https://www.shockedfuturestudios.com/opengraph.png',
+        width: 3840,
+        height: 2160,
+        alt: 'Shocked Future Open Graph Image',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
@@ -23,6 +40,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <GoogleTagManager gtmId="G-WWJDEHC05F" />
+      <head>
+        <GoogleAdSense />
+      </head>
       <body className={inter.className}>
 
         <AuthProvider>
