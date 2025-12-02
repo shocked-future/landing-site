5fe4aaa03187a660eeddfc15af3c8b955a0e58e1
Quentin Smith <quentin@shockedfuturestudios.com>
2025-12-02 15:58:19 -0600

    Replace with Google Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google'
 import { AuthProvider } from '@/context/AuthContext';
 
 
@@ -22,14 +22,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="G-WWJDEHC05F" />
       <body className={inter.className}>
+
         <AuthProvider>
           <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
             <Navbar />
             <main style={{ flex: 1 }}>{children}</main>
             <Footer />
             <SpeedInsights />
-            <GoogleAnalytics gaId="G-WWJDEHC05F" />
           </div>
         </AuthProvider>
       </body>
